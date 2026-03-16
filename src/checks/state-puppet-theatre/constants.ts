e9c589cd36b1a19b51c1a4847d34edccfb2f3374
Vlad Solovey <connectwithvlad@gmail.com>
2026-03-16 22:11:24 +0300

    feat: revert check url

--- src/checks/state-puppet-theatre/constants.ts
+++ src/checks/state-puppet-theatre/constants.ts
@@ -1,2 +1,2 @@
-export const AFFICHE_URL = "https://puppet-minsk.by/bilety/afisha";
+export const AFFICHE_URL = "https://puppet-minsk.by/afisha";
 export const DATA_FILE_NAME = "state-puppet-theatre.json";
